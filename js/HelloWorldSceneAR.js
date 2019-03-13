90eab80682f19f54d86e149dd3eb867f9d49831c
Justine Lee <justinelee103@gmail.com>
2019-03-13 18:03:34 -0400

    still working on rotate method

--- js/HelloWorldSceneAR.js
+++ js/HelloWorldSceneAR.js
@@ -24,13 +24,40 @@ export default class HelloWorldSceneAR extends Component {
 
     // Set initial state here
     this.state = {
-      text : "Initializing AR..."
+      text : "Initializing AR...",
+      rotation: [0,0,0]
     };
 
     // bind 'this' to functions
     this._onInitialized = this._onInitialized.bind(this);
+    this._onRotate = this._onRotate.bind(this)
+  }
+  _onInitialized(state, reason) {
+    if (state == ViroConstants.TRACKING_NORMAL) {
+      this.setState({
+        text : "Hello World!"
+      });
+    } else if (state == ViroConstants.TRACKING_NONE) {
+      // Handle loss of tracking
+    }
   }
 
+  _onRotate(rotateState, rotationFactor, source) {
+
+    console.log('rotateState', rotateState, 'rotatationFactor', rotationFactor, 'source', source)
+    if (rotateState == 3) {
+      this.setState({
+        rotation : [this.state.rotation[0], this.state.rotation[1] + rotationFactor, this.state.rotation[2]]
+      });
+      //this.props.onClickStateCallback(this.props.modelIDProps.uuid, rotateState, UIConstants.LIST_MODE_MODEL);
+    
+      return;
+    }
+
+    //this.arNodeRef.setNativeProps({rotation:[this.state.rotation[0], this.state.rotation[1] + rotationFactor, this.state.rotation[2]]});
+  }
+
+  
   render() {
     return (
       <ViroARScene onTrackingUpdated={this._onInitialized} >
@@ -45,12 +72,15 @@ export default class HelloWorldSceneAR extends Component {
           
         <ViroNode>
           <Viro3DObject dragType="FixedDistance" onDrag={()=>{}}
+              onRotate={()=>{}}
               source={require('./res/emoji_smile/emoji_smile.vrx')}
               resources={[require('./res/emoji_smile/emoji_smile_diffuse.png'),
                   require('./res/emoji_smile/emoji_smile_normal.png'),
                   require('./res/emoji_smile/emoji_smile_specular.png')]}
               position={[-.5, .5, -1]}
               scale={[.2, .2, .2]}
+              // rotation={this.state.rotation}
+              // onRotate={this._onRotate}
               type="VRX" />
 
         {/* stool below */}
@@ -72,15 +102,9 @@ export default class HelloWorldSceneAR extends Component {
     );
   }
 
-  _onInitialized(state, reason) {
-    if (state == ViroConstants.TRACKING_NORMAL) {
-      this.setState({
-        text : "Hello World!"
-      });
-    } else if (state == ViroConstants.TRACKING_NONE) {
-      // Handle loss of tracking
-    }
-  }
+
+
+
 }
 
 var styles = StyleSheet.create({
