cc0bef914b1c7afa48338d799dd2e8cdac101ec8
Bianca N <bianca.neptune@gmail.com>
2019-03-18 12:06:05 -0400

    added key

--- App.js
+++ App.js
@@ -7,7 +7,7 @@
  * of patent rights can be found in the PATENTS file in the same directory.
  */
 
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import {
   AppRegistry,
   Text,
@@ -16,31 +16,29 @@ import {
   PixelRatio,
   TouchableHighlight,
   Image
-} from 'react-native';
+} from "react-native";
 
 import {
   ViroBox,
   ViroMaterials,
   ViroVRSceneNavigator,
   ViroARSceneNavigator
-} from 'react-viro'
+} from "react-viro";
 
-
-import { Overlay } from 'react-native-elements'
-import AllProducts from './js/AllProductPage'
-import FavoritesPage from './js/FavoritesPage'
-import Icon from 'react-native-vector-icons/FontAwesome'
-
+import { Overlay } from "react-native-elements";
+import AllProducts from "./js/AllProductPage";
+import FavoritesPage from "./js/FavoritesPage";
+import Icon from "react-native-vector-icons/FontAwesome";
 
 /*
  TODO: Insert your API key below
  */
 var sharedProps = {
-  apiKey: "7C313AAF-F252-430D-9124-1B1DF5CE1CA2",
-}
+  apiKey: "7C313AAF-F252-430D-9124-1B1DF5CE1CA2"
+};
 
 // Sets the default scene you want for AR and VR
-var InitialARScene = require('./js/HelloWorldSceneAR')
+var InitialARScene = require("./js/HelloWorldSceneAR");
 
 // This determines which type of experience to launch in, or UNSET, if the user should
 // be presented with a choice of AR or VR. By default, we offer the user a choice.
@@ -51,106 +49,111 @@ export default class ViroSample extends Component {
     super();
 
     this.state = {
-      sharedProps : sharedProps,
+      sharedProps: sharedProps,
 
       isVisible: false,
       visibleFavorites: false
-    }
+    };
   }
 
   productsButton = () => {
-    this.setState({isVisible: true})
-  }
+    this.setState({ isVisible: true });
+  };
 
   favoritesButton = () => {
     this.setState({
       visibleFavorites: true
-    })
-  }
+    });
+  };
 
   // Replace this function with the contents of _getVRNavigator() or _getARNavigator()
   // if you are building a specific type of experience.
   render() {
-    return(
-      <View style={localStyles.outer} >
-
-        <ViroARSceneNavigator style={localStyles.arView} {...this.state.sharedProps}
-          initialScene={{scene: InitialARScene}} />
+    return (
+      <View style={localStyles.outer}>
+        <ViroARSceneNavigator
+          style={localStyles.arView}
+          {...this.state.sharedProps}
+          initialScene={{ scene: InitialARScene }}
+        />
 
         <View style={localStyles.navBar}>
-          <TouchableHighlight underlayColor={'#00000000'} 
-            onPress={this.productsButton}>
+          <TouchableHighlight
+            underlayColor={"#00000000"}
+            onPress={this.productsButton}
+          >
             <Image source={require("./js/res/btn_mode_objects.png")} />
           </TouchableHighlight>
 
-          <TouchableHighlight underlayColor={'#00000000'} >
-            onPress={this.favoritesButton}>
+          <TouchableHighlight
+            underlayColor={"#00000000"}
+            onPress={this.favoritesButton}
+          >
             <Image source={require("./js/res/btn_mode_objects.png")} />
           </TouchableHighlight>
 
-          <TouchableHighlight underlayColor={'#00000000'} >
+          <TouchableHighlight underlayColor={"#00000000"}>
             <Image source={require("./js/res/btn_mode_objects.png")} />
           </TouchableHighlight>
         </View>
 
-        <Overlay 
-          isVisible={this.state.isVisible} 
-
+        <Overlay
+          isVisible={this.state.isVisible}
           overlayBackgroundColor="#ACC6C7"
-
           // windowBackgroundColor="gray"
           width="auto"
           height="auto"
-          onBackdropPress={() => this.setState({ isVisible: false })}>
-            <AllProducts />
+          onBackdropPress={() => this.setState({ isVisible: false })}
+        >
+          <AllProducts />
         </Overlay>
 
-        <Overlay 
-          isVisible={this.state.visibleFavorites} 
+        <Overlay
+          isVisible={this.state.visibleFavorites}
           overlayBackgroundColor="#E5E8E9"
           width="auto"
           height={700}
-          onBackdropPress={() => this.setState({ visibleFavorites: false })}>
-            <FavoritesPage />
-        </Overlay> 
-
+          onBackdropPress={() => this.setState({ visibleFavorites: false })}
+        >
+          <FavoritesPage />
+        </Overlay>
       </View>
-    )
+    );
   }
 }
 
 var localStyles = StyleSheet.create({
-  outer : {
-    flex : 1,
+  outer: {
+    flex: 1
   },
 
   arView: {
-    flex:1,
+    flex: 1
   },
 
-  buttons : {
+  buttons: {
     height: 80,
     width: 80,
-    paddingTop:20,
-    paddingBottom:20,
+    paddingTop: 20,
+    paddingBottom: 20,
     marginTop: 10,
     marginBottom: 10,
-    backgroundColor:'#f0f8ff',
+    backgroundColor: "#f0f8ff",
     borderRadius: 10,
     borderWidth: 1,
     // borderColor: '#ffffff00',
-    borderColor: '#00000000'
+    borderColor: "#00000000"
   },
 
   navBar: {
     flex: 1,
-    flexDirection: 'row',
+    flexDirection: "row",
     justifyContent: "space-around",
-    position: 'absolute',  
-    left: 0, 
-    right: 0, 
+    position: "absolute",
+    left: 0,
+    right: 0,
     bottom: 77
   }
 });
 
-module.exports = ViroSample
+module.exports = ViroSample;
--- js/FavoritesPage.js
+++ js/FavoritesPage.js
@@ -1,47 +1,62 @@
 "use strict";
 
 import React, { Component } from "react";
-import { StyleSheet, View, Text, Image, FlatList } from "react-native"
-import { Card } from "react-native-elements"
+import { StyleSheet, View, Text, Image, FlatList } from "react-native";
+import { Card } from "react-native-elements";
 
 export default class FavoritesPage extends Component {
   constructor() {
     super();
     this.state = {
-      favorites: [{
-              "name": "assets/3txPAhYeu-x",
-              "displayName": "Office Chair",
-              "authorName": "Ryan Donaldson",
-              "objurl": "https://poly.googleapis.com/downloads/fp/1551711596400293/3txPAhYeu-x/3DjXPukLzqV/model.obj",
-              "mtlurl": "https://poly.googleapis.com/downloads/fp/1551711596400293/3txPAhYeu-x/3DjXPukLzqV/materials.mtl",
-              "formatType": "OBJ",
-              "thumbnail": "https://lh3.googleusercontent.com/dlwexhUXTVDfcmJayQRruP6IqK35UG1hw5C8Wmv-5E7d-cAfVQf5ThJ1cmiayHc"
-          },
-          {
-              "name": "assets/7Q_Ab2HLll1",
-              "displayName": "Couch | Wde",
-              "authorName": "Danny Bittman",
-              "objurl": "https://poly.googleapis.com/downloads/fp/1552556729377784/7Q_Ab2HLll1/c7YDClm08KI/model.obj",
-              "mtlurl": "https://poly.googleapis.com/downloads/fp/1552556729377784/7Q_Ab2HLll1/c7YDClm08KI/materials.mtl",
-              "formatType": "OBJ",
-              "thumbnail": "https://lh3.googleusercontent.com/XyxaRG8WVfk1Iirhe5SLZ5M_G164xBiOQVSUYY78mCnIZFaSIoMkmHvkrpe6I5Hgzg"
-          }, {
-              "name": "assets/3txPAhYeu-x",
-              "displayName": "Office Chair",
-              "authorName": "Ryan Donaldson",
-              "objurl": "https://poly.googleapis.com/downloads/fp/1551711596400293/3txPAhYeu-x/3DjXPukLzqV/model.obj",
-              "mtlurl": "https://poly.googleapis.com/downloads/fp/1551711596400293/3txPAhYeu-x/3DjXPukLzqV/materials.mtl",
-              "formatType": "OBJ",
-              "thumbnail": "https://lh3.googleusercontent.com/dlwexhUXTVDfcmJayQRruP6IqK35UG1hw5C8Wmv-5E7d-cAfVQf5ThJ1cmiayHc"
-          }, {
-              "name": "assets/7Q_Ab2HLll1",
-              "displayName": "Couch | Wde",
-              "authorName": "Danny Bittman",
-              "objurl": "https://poly.googleapis.com/downloads/fp/1552556729377784/7Q_Ab2HLll1/c7YDClm08KI/model.obj",
-              "mtlurl": "https://poly.googleapis.com/downloads/fp/1552556729377784/7Q_Ab2HLll1/c7YDClm08KI/materials.mtl",
-              "formatType": "OBJ",
-              "thumbnail": "https://lh3.googleusercontent.com/XyxaRG8WVfk1Iirhe5SLZ5M_G164xBiOQVSUYY78mCnIZFaSIoMkmHvkrpe6I5Hgzg"
-          }
+      favorites: [
+        {
+          name: "assets/3txPAhYeu-x",
+          displayName: "Office Chair",
+          authorName: "Ryan Donaldson",
+          objurl:
+            "https://poly.googleapis.com/downloads/fp/1551711596400293/3txPAhYeu-x/3DjXPukLzqV/model.obj",
+          mtlurl:
+            "https://poly.googleapis.com/downloads/fp/1551711596400293/3txPAhYeu-x/3DjXPukLzqV/materials.mtl",
+          formatType: "OBJ",
+          thumbnail:
+            "https://lh3.googleusercontent.com/dlwexhUXTVDfcmJayQRruP6IqK35UG1hw5C8Wmv-5E7d-cAfVQf5ThJ1cmiayHc"
+        },
+        {
+          name: "assets/7Q_Ab2HLll1",
+          displayName: "Couch | Wde",
+          authorName: "Danny Bittman",
+          objurl:
+            "https://poly.googleapis.com/downloads/fp/1552556729377784/7Q_Ab2HLll1/c7YDClm08KI/model.obj",
+          mtlurl:
+            "https://poly.googleapis.com/downloads/fp/1552556729377784/7Q_Ab2HLll1/c7YDClm08KI/materials.mtl",
+          formatType: "OBJ",
+          thumbnail:
+            "https://lh3.googleusercontent.com/XyxaRG8WVfk1Iirhe5SLZ5M_G164xBiOQVSUYY78mCnIZFaSIoMkmHvkrpe6I5Hgzg"
+        },
+        {
+          name: "assets/3txPAhYeu-x",
+          displayName: "Office Chair",
+          authorName: "Ryan Donaldson",
+          objurl:
+            "https://poly.googleapis.com/downloads/fp/1551711596400293/3txPAhYeu-x/3DjXPukLzqV/model.obj",
+          mtlurl:
+            "https://poly.googleapis.com/downloads/fp/1551711596400293/3txPAhYeu-x/3DjXPukLzqV/materials.mtl",
+          formatType: "OBJ",
+          thumbnail:
+            "https://lh3.googleusercontent.com/dlwexhUXTVDfcmJayQRruP6IqK35UG1hw5C8Wmv-5E7d-cAfVQf5ThJ1cmiayHc"
+        },
+        {
+          name: "assets/7Q_Ab2HLll1",
+          displayName: "Couch | Wde",
+          authorName: "Danny Bittman",
+          objurl:
+            "https://poly.googleapis.com/downloads/fp/1552556729377784/7Q_Ab2HLll1/c7YDClm08KI/model.obj",
+          mtlurl:
+            "https://poly.googleapis.com/downloads/fp/1552556729377784/7Q_Ab2HLll1/c7YDClm08KI/materials.mtl",
+          formatType: "OBJ",
+          thumbnail:
+            "https://lh3.googleusercontent.com/XyxaRG8WVfk1Iirhe5SLZ5M_G164xBiOQVSUYY78mCnIZFaSIoMkmHvkrpe6I5Hgzg"
+        }
       ]
     };
   }
@@ -50,16 +65,18 @@ export default class FavoritesPage extends Component {
     return (
       <View style={styles.listContainer}>
         <Text style={styles.AllProductPage}>Favorites</Text>
-        <FlatList 
-            data={this.state.favorites} 
-            renderItem={({item, index}) => 
-                <Card key={index}>
-                    <Text>Name: {item.displayName}</Text>
-                    <Image style={{ width: 200, height: 200 }}
-                    source={{ uri: item.thumbnail }}
+        <FlatList
+          data={this.state.favorites}
+          renderItem={({ item, index }) => (
+            <Card key={index}>
+              <Text>Name: {item.displayName}</Text>
+              <Image
+                style={{ width: 200, height: 200 }}
+                source={{ uri: item.thumbnail }}
               />
-                </Card>}
-            //keyExtractor={item => item.id_str}
+            </Card>
+          )}
+          keyExtractor={(item, index) => index}
         />
       </View>
     );
@@ -82,4 +99,3 @@ var styles = StyleSheet.create({
     flex: 1
   }
 });
-
