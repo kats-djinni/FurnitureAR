346e706c61da076bd95f8cd7c9bd70e9d3a74090
Bianca N <bianca.neptune@gmail.com>
2019-03-23 19:18:25 -0400

    pinch scale equation

--- js/SingleProduct.js
+++ js/SingleProduct.js
@@ -1,6 +1,12 @@
 import React, { Component } from "react";
 
-import { ViroNode, Viro3DObject, ViroARPlaneSelector, ViroARPlane } from "react-viro";
+import {
+  ViroNode,
+  Viro3DObject,
+  ViroARPlaneSelector,
+  ViroARPlane,
+  ViroText
+} from "react-viro";
 
 export default class SingleProduct extends Component {
   constructor(props) {
@@ -27,6 +33,7 @@ export default class SingleProduct extends Component {
 
   _onPinch(pinchState, scaleFactor, source) {
     if (pinchState === 3) {
+      console.log("scale", this.state.scale);
       this.setState({
         scale: [
           this.state.scale[0] * scaleFactor,
@@ -37,30 +44,49 @@ export default class SingleProduct extends Component {
     }
   }
 
-  
-  
   render() {
+    //placeholder
+    // const initialScale = this.props.initialScale
+    const pinchScale = (this.state.scale[0] / 0.75) * 100;
     return (
       <ViroNode onDrag={() => {}}>
-        <ViroARPlaneSelector>
-          <Viro3DObject
-            source={{
-              uri: this.props.item.objurl
-            }}
-            resources={[
-              {
-                uri: this.props.item
-              }
-            ]}
-            position={[0, -.5, 0]}
-            scale={this.state.scale}
-            rotation={this.state.rotation}
-            onPinch={this._onPinch}
-            onRotate={this._onRotate}
-            onClick={() => this.props.triggerItem(this.props.index)}
-            type="OBJ"
-          />
-        </ViroARPlaneSelector>
+        {/* <ViroARPlaneSelector> */}
+        <ViroText
+          text={Math.floor(pinchScale).toString() + "%"}
+          textAlign="left"
+          textAlignVertical="top"
+          textLineBreakMode="justify"
+          textClipMode="clipToBounds"
+          color="#ff0000"
+          width={2}
+          height={2}
+          style={{
+            fontFamily: "Arial",
+            fontSize: 20,
+            fontWeight: "400",
+            fontStyle: "italic",
+            color: "#0000FF"
+          }}
+          position={[0, 0, -5]}
+        />
+        <Viro3DObject
+          source={{
+            uri: this.props.item.objurl
+          }}
+          resources={[
+            {
+              uri: this.props.item
+            }
+          ]}
+          position={[0, -0.5, 0]}
+          scale={this.state.scale}
+          rotation={this.state.rotation}
+          onPinch={this._onPinch}
+          onRotate={this._onRotate}
+          onClick={() => this.props.triggerItem(this.props.index)}
+          type="OBJ"
+        />
+        {/* </ViroARPlaneSelector> */}
       </ViroNode>
     );
   }
