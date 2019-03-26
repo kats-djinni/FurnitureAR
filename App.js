f8200a69cc191c5208e250495ee908f3d8757770
Justine Lee <justinelee103@gmail.com>
2019-03-26 13:30:59 -0400

    Feat: Added searching for plan overlay

--- App.js
+++ App.js
@@ -52,7 +52,8 @@ export default class ViroSample extends Component {
       screenshotUrl:'',
       photoPreviewVisibility: false,
       isLoading: true,
-      visibleInstructions: false
+      visibleInstructions: false,
+      searchingPlaneIcon: false
     };
   
   }
@@ -84,6 +85,15 @@ export default class ViroSample extends Component {
   changeVisibility = () => {
     this.setState({ isVisible: !this.state.isVisible });
   };
+
+  searchingPlaneIconVisibility = () => {
+    this.setState({searchingPlaneIcon: !this.state.searchingPlaneIcon})
+    setTimeout(() => {
+      this.setState({
+        searchingPlaneIcon: !this.state.searchingPlaneIcon
+      })
+    }, 4000)
+  }
   
   triggerItemBar = (key) => {
     this.setState({
@@ -201,7 +211,17 @@ export default class ViroSample extends Component {
           height={Dimensions.get("window").height * 0.75}
           onBackdropPress={() => this.setState({ isVisible: false })}
         >
-          <AllProducts visibilityChange={this.changeVisibility} />
+          <AllProducts visibilityChange={this.changeVisibility} searchingPlaneIcon={this.searchingPlaneIconVisibility} />
+        </Overlay>
+
+       {/* Searching for plan icon after choosing a product from All products page */}
+        <Overlay
+          isVisible={this.state.searchingPlaneIcon}
+          overlayBackgroundColor = "#00000000"
+          width={Dimensions.get("window").width * 0.75}
+          height={Dimensions.get("window").height * 0.75}
+        >
+          <Text style={localStyles.savingMessage}>Scan the room and select placement!</Text>
         </Overlay>
 
         <Overlay
